a8ab4790fc57454f8af91427bae042010edb15e5
Jansen Okasaki <jansencruzyuki@gmail.com>
2025-10-04 13:29:32 +0000

    Task

--- ex01-express/api/routes/task.js
+++ ex01-express/api/routes/task.js
@@ -26,25 +26,16 @@ router.get("/:taskId", async (req, res) => {
 
 router.post("/", async (req, res) => {
   try {
-    console.log("req.body:", req.body);
-    console.log("req.context.me:", req.context.me);
-    if (!req.context.me) {
-      return res.status(401).json({ error: "Usuário não autenticado" });
-    }
-    if (!req.body.text) {
-      return res.status(400).json({ error: "Texto da mensagem é obrigatório" });
-    }
+
+  
 
     // Use o ID do usuário autenticado ao invés do userId do body
-    const userId = req.context.me.id;
     console.log("userId type:", typeof userId, "value:", userId);
     
     const taskData = {
       id: uuidv4(),
       descricao: req.body.descrocao,
-      userId: userId,
     };
-    console.log("taskData:", messageData);
 
     const task = await req.context.models.Task.create(taskData);
 
